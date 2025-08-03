7f21e2fb75892e42b2b6f4ff69bda70074e908b2
Nishadsha91 <muthumattilnishad91@gmail.com>
2025-08-03 11:09:33 +0530

    Time: 1 ms (86.52%), Space: 57.1 MB (31.09%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var sumOfMultiples = function(n) {
+    let out = 0;
+
+    for(let i=1; i<=n ;i++){
+
+        if(i%3===0 || i % 5 === 0 || i % 7 === 0){
+            out+=i
+        }
+        
+    }
+    return out
+};
