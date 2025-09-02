f3799482b08a0fe20cc8df779e7bb3bff26fb0e8
Mohammed Nishad <muthumattilnishad91@gmail.com>
2025-09-02 23:28:18 +0530

    Time: 0 ms (100%), Space: 12.4 MB (86.79%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        s = pow(x,n)
+        return float(s)
+
+        
