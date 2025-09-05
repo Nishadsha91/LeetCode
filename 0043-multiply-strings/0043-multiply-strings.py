0dd26a2c11f7e002330d36ace7e01ab122d40a73
Mohammed Nishad <muthumattilnishad91@gmail.com>
2025-09-05 10:36:03 +0530

    Time: 0 ms (100%), Space: 12.6 MB (13.32%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        mul = int(num1) * int(num2)
+        return str(mul)
