79a057b88846cdeafac214e786bd349375ba8907
Mohammed Nishad <muthumattilnishad91@gmail.com>
2025-09-08 10:13:44 +0530

    Time: 67 ms (53.11%), Space: 20.1 MB (84.66%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -4,17 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # nums.sort() 
-        # smallest = 1  
-
-        # for num in nums:
-        #     if num == smallest:
-        #         smallest += 1
-
-        # return smallest
-
         n = len(nums)
-        
+
         for i in range(n):
             while 1 <= nums[i] <= n and nums[nums[i]-1] != nums[i]:
                 nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
