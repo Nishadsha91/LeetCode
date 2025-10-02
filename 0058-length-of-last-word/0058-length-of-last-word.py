9bfcc80b781d498ab91ed3903b51c84775d55263
Mohammed Nishad <muthumattilnishad91@gmail.com>
2025-10-02 12:25:11 +0530

    Time: 0 ms (100%), Space: 12.5 MB (45.36%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        w= s.strip().split()
+        return len(w[-1])
+
+        
