705e0cbd052ab8fb7f056c00b39c3dd115dddbd4
Mohammed Nishad <muthumattilnishad91@gmail.com>
2025-10-22 20:29:51 +0530

    Time: 37 ms (35.12%), Space: 12.3 MB (85.45%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.py
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         total = 0
 
-        for i in range(1,n+1):
+        for i in range(n+1):
             if i%3==0 or i%5==0 or i%7==0:
                 total += i
         return total
