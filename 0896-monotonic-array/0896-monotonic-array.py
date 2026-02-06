eedb6515a9b6c69f56d4688740ffbcb81faa3e3c
Mohammed Nishad <muthumattilnishad91@gmail.com>
2026-02-06 14:58:20 +0530

    Time: 135 ms (12.31%), Space: 20.6 MB (51.82%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isMonotonic(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        inc = True
+        dec = True
+        
+        for i in range(1,len(nums)):
+            if nums[i] > nums[i-1]:
+                inc = False
+            if nums[i] < nums[i-1]:
+                dec = False
+        return inc or dec
+
+        
